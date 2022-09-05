b04473baa8d0aef0b8407e7d7c5a8ba795778dbc
Rodolfo S Alexandre <r.santos.13@hotmail.com>
2022-09-05 08:15:16 -0300

    Remoção do front para separar

--- front/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosInstance } from "axios"
-
-const api: AxiosInstance = axios.create({
-    baseURL: 'https://student-registration-test.herokuapp.com'
-    //baseURL: 'http://localhost:3000'
-});
-
-export default api;
